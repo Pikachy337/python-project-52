25f96bc4f74a2d55b08b6ce1e284617a8b30e172
Dmitry <pikachyshkaa@gmail.com>
2025-04-10 14:18:26 +0500

    change location + filled models

---
+++ task_manager/labels/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from task_manager.tasks.models import Task
+
+
+class Label(models.Model):
+    name = models.CharField(
+        max_length=100,
+        unique=True,
+        verbose_name=_('Name')
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    tasks = models.ManyToManyField(
+        Task,
+        related_name='labels',
+        blank=True,
+        verbose_name=_('Tasks')
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _('Label')
+        verbose_name_plural = _('Labels')
