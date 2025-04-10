a9f60c3d64a358394f93a6aa6f3c8fcb215bc9a6
Dmitry <pikachyshkaa@gmail.com>
2025-04-10 16:25:45 +0500

    updating tests

--- task_manager/tasks/tests.py
+++ task_manager/tasks/tests.py
@@ -3,12 +3,13 @@ from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth import get_user_model
 from task_manager.statuses.models import Status
+from task_manager.labels.models import Label
 from .models import Task
 
 User = get_user_model()
 
 
-class TaskCRUDTest(TestCase):
+class TaskFilterTest(TestCase):
     def setUp(self):
         self.user = User.objects.create_user(
             username='testuser',
@@ -18,65 +19,57 @@ class TaskCRUDTest(TestCase):
             username='otheruser',
             password='testpass123'
         )
-        self.status = Status.objects.create(name='Test Status')
-        self.task = Task.objects.create(
-            name='Test Task',
-            description='Test Description',
-            status=self.status,
-            author=self.user
+        self.status1 = Status.objects.create(name='Status 1')
+        self.status2 = Status.objects.create(name='Status 2')
+        self.label1 = Label.objects.create(name='Label 1')
+        self.label2 = Label.objects.create(name='Label 2')
+
+        self.task1 = Task.objects.create(
+            name='Task 1',
+            description='Desc 1',
+            status=self.status1,
+            author=self.user,
+            executor=self.other_user
         )
+        self.task1.labels.add(self.label1)
+
+        self.task2 = Task.objects.create(
+            name='Task 2',
+            description='Desc 2',
+            status=self.status2,
+            author=self.other_user,
+            executor=self.user
+        )
+        self.task2.labels.add(self.label2)
+
+    def test_filter_by_status(self):
         self.client.login(username='testuser', password='testpass123')
-
-    def test_task_list_view(self):
-        response = self.client.get(reverse('tasks:list'))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'Test Task')
-
-    def test_task_create_view(self):
-        response = self.client.post(reverse('tasks:create'), {
-            'name': 'New Task',
-            'description': 'New Description',
-            'status': self.status.id,
-        })
-        self.assertEqual(response.status_code, 302)
-        self.assertTrue(Task.objects.filter(name='New Task').exists())
-
-    def test_task_update_view(self):
-        response = self.client.post(
-            reverse('tasks:update', args=[self.task.id]),
-            {
-                'name': 'Updated Task',
-                'description': 'Updated Description',
-                'status': self.status.id,
-            }
+        response = self.client.get(
+            reverse('tasks:list') + f'?status={self.status1.id}'
         )
-        self.assertEqual(response.status_code, 302)
-        self.task.refresh_from_db()
-        self.assertEqual(self.task.name, 'Updated Task')
+        self.assertContains(response, 'Task 1')
+        self.assertNotContains(response, 'Task 2')
 
-    def test_task_delete_view(self):
-        response = self.client.post(
-            reverse('tasks:delete', args=[self.task.id])
+    def test_filter_by_executor(self):
+        self.client.login(username='testuser', password='testpass123')
+        response = self.client.get(
+            reverse('tasks:list') + f'?executor={self.user.id}'
         )
-        self.assertEqual(response.status_code, 302)
-        self.assertFalse(Task.objects.filter(id=self.task.id).exists())
+        self.assertContains(response, 'Task 2')
+        self.assertNotContains(response, 'Task 1')
 
-    def test_task_delete_by_non_author(self):
-        self.client.login(username='otheruser', password='testpass123')
-        response = self.client.post(
-            reverse('tasks:delete', args=[self.task.id])
+    def test_filter_by_label(self):
+        self.client.login(username='testuser', password='testpass123')
+        response = self.client.get(
+            reverse('tasks:list') + f'?labels={self.label1.id}'
         )
-        self.assertEqual(response.status_code, 302)
-        self.assertTrue(Task.objects.filter(id=self.task.id).exists())
+        self.assertContains(response, 'Task 1')
+        self.assertNotContains(response, 'Task 2')
 
-
-class TaskAccessTest(TestCase):
-    def test_unauthenticated_access(self):
-        urls = [
-            reverse('tasks:list'),
-            reverse('tasks:create'),
-        ]
-        for url in urls:
-            response = self.client.get(url)
-            self.assertEqual(response.status_code, 302)
-            self.assertRedirects(response, f'/login/?next={url}')
+    def test_filter_self_tasks(self):
+        self.client.login(username='testuser', password='testpass123')
+        response = self.client.get(
+            reverse('tasks:list') + '?self_tasks=on'
+        )
+        self.assertContains(response, 'Task 1')
+        self.assertNotContains(response, 'Task 2')
