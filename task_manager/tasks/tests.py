0dc3b97e8884a224a00e383e17896e1dc84c1645
Dmitry <pikachyshkaa@gmail.com>
2025-04-10 13:22:57 +0500

    add tests

--- task_manager/tasks/tests.py
+++ task_manager/tasks/tests.py
@@ -1,3 +1,82 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth import get_user_model
+from task_manager.statuses.models import Status
+from .models import Task
 
-# Create your tests here.
+User = get_user_model()
+
+
+class TaskCRUDTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='testpass123'
+        )
+        self.other_user = User.objects.create_user(
+            username='otheruser',
+            password='testpass123'
+        )
+        self.status = Status.objects.create(name='Test Status')
+        self.task = Task.objects.create(
+            name='Test Task',
+            description='Test Description',
+            status=self.status,
+            author=self.user
+        )
+        self.client.login(username='testuser', password='testpass123')
+
+    def test_task_list_view(self):
+        response = self.client.get(reverse('tasks:list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Test Task')
+
+    def test_task_create_view(self):
+        response = self.client.post(reverse('tasks:create'), {
+            'name': 'New Task',
+            'description': 'New Description',
+            'status': self.status.id,
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(Task.objects.filter(name='New Task').exists())
+
+    def test_task_update_view(self):
+        response = self.client.post(
+            reverse('tasks:update', args=[self.task.id]),
+            {
+                'name': 'Updated Task',
+                'description': 'Updated Description',
+                'status': self.status.id,
+            }
+        )
+        self.assertEqual(response.status_code, 302)
+        self.task.refresh_from_db()
+        self.assertEqual(self.task.name, 'Updated Task')
+
+    def test_task_delete_view(self):
+        response = self.client.post(
+            reverse('tasks:delete', args=[self.task.id])
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertFalse(Task.objects.filter(id=self.task.id).exists())
+
+    def test_task_delete_by_non_author(self):
+        self.client.login(username='otheruser', password='testpass123')
+        response = self.client.post(
+            reverse('tasks:delete', args=[self.task.id])
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(Task.objects.filter(id=self.task.id).exists())
+
+
+class TaskAccessTest(TestCase):
+    def test_unauthenticated_access(self):
+        urls = [
+            reverse('tasks:list'),
+            reverse('tasks:create'),
+        ]
+        for url in urls:
+            response = self.client.get(url)
+            self.assertEqual(response.status_code, 302)
+            self.assertRedirects(response, f'/login/?next={url}')
