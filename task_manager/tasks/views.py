3d6be3862b1296d170e0cfccb4d7d3b466ee9b9f
Dmitry <pikachyshkaa@gmail.com>
2025-04-10 16:21:55 +0500

    updating the view

--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -9,12 +9,27 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Task
 from .forms import TaskForm
 from django.contrib.auth.mixins import UserPassesTestMixin
+from django_filters.views import FilterView
+from .filters import TaskFilter
 
 
-class TaskListView(LoginRequiredMixin, ListView):
+class TaskListView(LoginRequiredMixin, FilterView):
     model = Task
     template_name = 'tasks/list.html'
     context_object_name = 'tasks'
+    filterset_class = TaskFilter
+
+    def get_queryset(self):
+        return Task.objects.all().order_by('-created_at')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filter'] = TaskFilter(
+            self.request.GET,
+            queryset=self.get_queryset(),
+            request=self.request
+        )
+        return context
 
 
 class TaskCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
