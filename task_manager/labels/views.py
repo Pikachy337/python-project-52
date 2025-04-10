8cfa4a3ea8dcba2ec50b9f0bd75a5e4b5d9297ba
Dmitry <pikachyshkaa@gmail.com>
2025-04-10 14:22:56 +0500

    add labels-views

--- task_manager/labels/views.py
+++ task_manager/labels/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+from django.utils.translation import gettext_lazy as _
+from django.views.generic import (
+    ListView, CreateView, UpdateView, DeleteView
+)
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+from .forms import LabelForm
+from .models import Label
+
+
+class LabelListView(LoginRequiredMixin, ListView):
+    model = Label
+    template_name = 'labels/list.html'
+    context_object_name = 'labels'
+
+
+class LabelCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
+    model = Label
+    form_class = LabelForm
+    template_name = 'labels/create.html'
+    success_url = reverse_lazy('labels:list')
+    success_message = _('Label created successfully')
+
+
+class LabelUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
+    model = Label
+    form_class = LabelForm
+    template_name = 'labels/update.html'
+    success_url = reverse_lazy('labels:list')
+    success_message = _('Label updated successfully')
+
+
+class LabelDeleteView(LoginRequiredMixin, SuccessMessageMixin, DeleteView):
+    model = Label
+    template_name = 'labels/delete.html'
+    success_url = reverse_lazy('labels:list')
+    success_message = _('Label deleted successfully')
+
+    def form_valid(self, form):
+        if self.get_object().tasks.exists():
+            messages.error(
+                self.request,
+                _("Cannot delete label because it's in use")
+            )
+            return self.render_to_response(self.get_context_data())
+        return super().form_valid(form)
