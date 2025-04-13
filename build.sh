d585af6c9eaf50e10134d757f4fb2d97d1130282
Dmitry <pikachyshkaa@gmail.com>
2025-04-13 18:38:42 +0500

    update build

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
+set -o errexit
 
-make install && make migrate
+uv sync --all-packages
+
+uv run python manage.py collectstatic --no-input
+
+uv run python manage.py migrate
