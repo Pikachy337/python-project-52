3127337dd11fd92af7db414f8ebeb2f0b73829da
Dmitry <pikachyshkaa@gmail.com>
2025-04-15 10:28:34 +0500

    add dotenv

--- task_manager/tests/conftest.py
+++ task_manager/tests/conftest.py
@@ -1,6 +1,8 @@
 import pytest
 from django.core.management import call_command
+from dotenv import load_dotenv
 
+load_dotenv()
 
 @pytest.fixture(scope='session')
 def django_db_setup(django_db_setup, django_db_blocker):
