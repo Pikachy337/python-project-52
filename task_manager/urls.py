c11d2d8b7f1edcfbe6a4a8734f4fe19b171e5521
Dmitry <pikachyshkaa@gmail.com>
2025-04-18 16:26:51 +0500

    test

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -18,14 +18,12 @@ from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
 
-from task_manager.views import CustomLoginView, LogoutUser
+from task_manager.views import LoginUser, LogoutUser
 
 urlpatterns = [
     path('i18n/', include('django.conf.urls.i18n')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
-    path('login/',
-         CustomLoginView.as_view(template_name='general/general_form.html'),
-         name='login'),
+    path('login/', LoginUser.as_view(), name='login'),
     path('logout/', LogoutUser.as_view(), name='logout'),
     path('users/', include('task_manager.users.urls')),
     path('statuses/', include('task_manager.statuses.urls')),
--- task_manager/views.py
+++ task_manager/views.py
@@ -1,14 +1,12 @@
 from django.contrib import messages
 from django.contrib.auth import logout
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.views import LoginView
-from django.db import models
+from django.contrib.messages.views import SuccessMessageMixin
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
-from django.utils.decorators import method_decorator
 from django.utils.translation import gettext_lazy as _
 from django.views import View
-from django.views.decorators.http import require_POST
 from django.views.generic import TemplateView
 
 
@@ -21,55 +19,16 @@ class HomeView(TemplateView):
         return context
 
 
-class CustomLoginView(LoginView):
+class LoginUser(SuccessMessageMixin, LoginView):
+    form_class = AuthenticationForm
     template_name = 'general/general_form.html'
-    success_message = _('You have successfully logged in')
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = _('Login')
-        context['button'] = _('Enter')
-        return context
-
-    def get_success_url(self):
-        messages.success(self.request, self.success_message)
-        return reverse_lazy('home')
+    extra_context = {'title': _("Login"), 'button': _("Enter")}
+    success_message = _('You were login')
 
 
-@method_decorator(require_POST, name='dispatch')
 class LogoutUser(View):
-    def post(self, request, *args, **kwargs):
+
+    def get(self, request, *args, **kwargs):
         logout(request)
-        messages.info(request, _("You were logged out"))
+        messages.info(request, _("You were logout"))
         return redirect(reverse_lazy('home'))
-
-
-class BaseView(LoginRequiredMixin):
-    success_message = None
-
-    def form_valid(self, form):
-        messages.success(self.request, self.success_message)
-        return super().form_valid(form)
-
-
-class OwnerOnlyMixin(UserPassesTestMixin):
-    permission_denied_message = _("You don't have permission to do this")
-
-    def test_func(self):
-        return self.get_object().author == self.request.user
-
-    def handle_no_permission(self):
-        messages.error(self.request, self.permission_denied_message)
-        return super().handle_no_permission()
-
-
-class ProtectedDeleteMixin:
-    protected_message = None
-    protected_url = None
-
-    def form_valid(self, form):
-        try:
-            return super().form_valid(form)
-        except models.ProtectedError:
-            messages.error(self.request, self.protected_message)
-            return redirect(self.protected_url)
