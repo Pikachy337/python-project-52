d3ceba3561f6027b4f6b351274454dd2575d7336
Dmitry <pikachyshkaa@gmail.com>
2025-04-26 01:24:54 +0500

    test

--- task_manager/tasks/filters.py
+++ task_manager/tasks/filters.py
@@ -1,40 +1,36 @@
 import django_filters
 from django import forms
-from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
 
 from task_manager.labels.models import Label
 from task_manager.statuses.models import Status
-
-from .models import Task
-
-User = get_user_model()
+from task_manager.tasks.models import Task
+from task_manager.users.models import User
 
 
 class TaskFilter(django_filters.FilterSet):
     status = django_filters.ModelChoiceFilter(
         queryset=Status.objects.all(),
         label=_('Status'),
-        widget=forms.Select(attrs={'class': 'form-select'})
+        widget=forms.Select(attrs={'class': 'form-select'}),
     )
 
     executor = django_filters.ModelChoiceFilter(
         queryset=User.objects.all(),
         label=_('Executor'),
-        widget=forms.Select(attrs={'class': 'form-select'})
+        widget=forms.Select(attrs={'class': 'form-select'}),
     )
 
     labels = django_filters.ModelChoiceFilter(
         queryset=Label.objects.all(),
         label=_('Label'),
         widget=forms.Select(attrs={'class': 'form-select'}),
-        method='filter_labels'
     )
 
     self_tasks = django_filters.BooleanFilter(
         method='filter_self_tasks',
+        widget=forms.CheckboxInput(attrs={'class': 'form-check-input'}),
         label=_('Only my tasks'),
-        widget=forms.CheckboxInput()
     )
 
     class Meta:
@@ -44,15 +40,8 @@ class TaskFilter(django_filters.FilterSet):
     def __init__(self, *args, **kwargs):
         self.user = kwargs.pop('user', None)
         super().__init__(*args, **kwargs)
-        self.filters['executor'].field.label_from_instance\
-            = lambda obj: obj.get_full_name()
 
     def filter_self_tasks(self, queryset, name, value):
-        if value and self.user:
+        if value and self.user and self.user.is_authenticated:
             return queryset.filter(author=self.user)
-        return queryset
-
-    def filter_labels(self, queryset, name, value):
-        if value:
-            return queryset.filter(labels=value)
         return queryset
