d853fddd9cd4b5dd957e91d49f8320c978ff01b2
Dmitry <pikachyshkaa@gmail.com>
2025-04-30 18:24:18 +0500

    fix 3/3

--- task_manager/labels/views.py
+++ task_manager/labels/views.py
@@ -1,6 +1,6 @@
 from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
-from django.db.models import ProtectedError
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
@@ -49,25 +49,25 @@ class LabelUpdateView(SuccessMessageMixin, UpdateView):
         return context
 
 
-class LabelDeleteView(SuccessMessageMixin, DeleteView):
+class LabelDeleteView(LoginRequiredMixin, DeleteView):
     model = Label
     template_name = 'general/general_delete_confirm.html'
     success_url = reverse_lazy('labels:list')
     success_message = _('Label successfully deleted')
+    error_message = _('Cannot delete label')
+
+    def form_valid(self, form):
+        if self.get_object().tasks.exists():
+            messages.error(self.request, self.error_message)
+            return redirect(self.success_url)
+
+        messages.success(self.request, self.success_message)
+        return super().form_valid(form)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['button'] = _('Yes, delete')
+        context.update({
+            'title': _('Delete label'),
+            'button': _('Yes, delete')
+        })
         return context
-
-    def post(self, request, *args, **kwargs):
-        try:
-            self.delete(request, *args, **kwargs)
-            messages.success(self.request, _("Label was deleted successfully"))
-            return redirect(self.success_url)
-        except ProtectedError:
-            messages.error(
-                self.request,
-                _("Can't delete, label")
-            )
-            return redirect(self.success_url)
